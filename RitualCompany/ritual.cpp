4fe5923c1598849adabd545f5c36b37d89b66f20
Max OK <maximkillz14@gmail.com>
2022-12-26 23:59:17 +0300

    Класс ProductInputScreen вынесен в отдельные файлы ProductInputScreen.h и ProductInputScreen.cpp

--- RitualCompany/ritual.cpp
+++ RitualCompany/ritual.cpp
@@ -19,25 +19,6 @@ char getaChar() // получение символа
 
 
 
-///////////метод класса ProductInputScreen//////////////////
-void ProductInputScreen::setProduct() // добавить данные о товаре
-{
-    cout << "Enter the name of the product/service: " << endl;
-    getaLine(pName);
-    cout << "Enter the article number (1234): " << endl;
-    cin >> art;
-    cin.ignore(80, '\n');
-    cout << "Enter the price (499.9): " << endl;
-    cin >> prc;
-    cin.ignore(80, '\n');
-    Product* ptrProduct = new Product(pName, art, prc); // создать товар
-    ptrProductList->insertProduct(ptrProduct); // занести в список товаров
-}
-//--------------------------------------------------------
-
-
-
-
 /////////////////методы класса RevenueRow/////////////////////
 RevenueRow::RevenueRow(int ar) : art(ar) //конструктор
 { //Алгоритм fill() помещает копию значения value (у нас это 0)
--- RitualCompany/ritual.h
+++ RitualCompany/ritual.h
@@ -7,33 +7,12 @@
 #include <numeric> //для accumulate() и fill()
 #include <iomanip>
 #include "productlist.h"
-
+#include "productinputscreen.h"
 using namespace std;
 
 //////////////////// глобальные методы //////////////////////
 void getaLine(string& inStr); // получение строки текста
 char getaChar(); // получение символа
-
-
-
-
-////////////////класс ProductInputScreen////////////////////
-//класс ProductInputScreen. Это класс, отвечающий за отображение «экрана»,
-//куда пользователь может ввести данные о новом товаре:
-class ProductInputScreen
-{
-private:
-    ProductList* ptrProductList;
-    string pName;
-    int art;
-    float prc;
-public:
-    ProductInputScreen(ProductList* ptrPL) : ptrProductList(ptrPL)
-    {
-        /* тут пусто */
-    }
-    void setProduct(); // добавить данные о товаре
-};
 
 
 
