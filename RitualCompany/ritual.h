c45ec70d49df41945ec4d0050b055d60e383845d
Max OK <maximkillz14@gmail.com>
2022-12-26 23:55:35 +0300

    Класс ProductList вынесен в отдельные файлы ProductList.h и ProductList.cpp

--- RitualCompany/ritual.h
+++ RitualCompany/ritual.h
@@ -6,7 +6,8 @@
 #include <string>
 #include <numeric> //для accumulate() и fill()
 #include <iomanip>
-#include "product.h"
+#include "productlist.h"
+
 using namespace std;
 
 //////////////////// глобальные методы //////////////////////
@@ -14,24 +15,6 @@ void getaLine(string& inStr); // получение строки текста
 char getaChar(); // получение символа
 
 
-
-///////////////////класс ProductList////////////////////////
-//класс ProductList — список всех товаров.
-//Он содержит множество указателей на класс Product
-//и оперирует ими при выводе
-class ProductList
-{
-private:
-    // установить указатели на товары
-    list <Product*> setPtrsProd; // указатели на класс Product
-    list <Product*>::iterator iter; //итератор
-public:
-    ~ProductList(); // деструктор (удаление товаров)
-    void insertProduct(Product*); // добавить товар в список
-    int getArt(string); // возвращает артикул товара
-    float getPrc(string); //возращает цену товара
-    void display(); // вывод списка товаров
-};
 
 
 ////////////////класс ProductInputScreen////////////////////
