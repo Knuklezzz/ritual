bd3bf0a3c8b5df223be654269c5317fee7f0dd19
Nikita Varov <nikitos.varov@yandex.ru>
2022-12-27 00:55:31 +0300

    Класс ExpenseInputScreen вынесен в отдельные файлы ExpenseInputScreen.h и ExpenseInputScreen.cpp

---
+++ RitualCompany/expenseinputscreen.h
@@ -0,0 +1,13 @@
+#pragma once
+#include "expenserecord.h"
+
+////////////////класс ExpenseInputScreen///////////////////
+//Класс для ввода расходов
+class ExpenseInputScreen
+{
+private:
+    ExpenseRecord* ptrExpenseRecord; // запись о расходах
+public:
+    ExpenseInputScreen(ExpenseRecord*);
+    void setExpense();
+};
